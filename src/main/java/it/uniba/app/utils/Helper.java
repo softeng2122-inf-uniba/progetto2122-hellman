f24f48813ec27dc21917c8837eddaf068f6e868b
Matteo Balice <matteobalice02@gmail.com>
2022-05-12 21:24:03 +0200

    Aggiunte enum per tentativo parola

--- src/main/java/it/uniba/app/utils/Helper.java
+++ src/main/java/it/uniba/app/utils/Helper.java
@@ -1,5 +1,12 @@
 package it.uniba.app.utils;
 
 public class Helper {
-    
+  final public static int FORMAT_LETTER_NEVER_TYPED = 0;
+  final public static int FORMAT_LETTER_NOT_FOUND = 1;
+  final public static int FORMAT_LETTER_FOUND_WRONG_POSITION = 2;
+  final public static int FORMAT_LETTER_FOUND_RIGHT_POSITION = 3;
+  
+  final public static int GAME_WIN = 5;
+  final public static int GAME_LOSE = 6;
+  final public static int GAME_WAITING = 7;
 }

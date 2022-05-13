21ec19eebf45c94df2061c4c5f014307d9a86c69
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2022-05-13 18:56:05 +0200

    Implementazione classe Helper
    Classe di supporto del gioco

--- src/main/java/it/uniba/app/utils/Helper.java
+++ src/main/java/it/uniba/app/utils/Helper.java
@@ -1,5 +1,18 @@
 package it.uniba.app.utils;
 
+/** 
+ * Classe di supporto che ha tutta una serie di costanti che servono per mostrare se le lettere della parola inserita
+ * dall'utente sono in posizione corretta oppure errata oppure per l'esito del gioco cioè se l'utente ha perso o vinto.
+*/
 public class Helper {
-    
+    final public static int FORMAT_LETTER_NEVER_TYPED = 0;
+    final public static int FORMAT_LETTER_NOT_FOUND = 1;
+    final public static int FORMAT_LETTER_FOUND_WRONG_POSITION = 2;
+    final public static int FORMAT_LETTER_FOUND_RIGHT_POSITION = 3;
+
+    final public static int GAME_WIN = 5;
+    final public static int GAME_LOSE = 6;
+    final public static int GAME_WAITING = 7;
+
+    private Helper(){}
 }

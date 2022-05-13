abc796ce49dc561c2a0c3b20424d0cb36e2e1a2e
Matteo Balice <matteobalice02@gmail.com>
2022-05-13 20:12:13 +0200

    Colori terminale

--- src/main/java/it/uniba/app/utils/Helper.java
+++ src/main/java/it/uniba/app/utils/Helper.java
@@ -6,6 +6,11 @@ public class Helper {
   final public static int FORMAT_LETTER_FOUND_WRONG_POSITION = 2;
   final public static int FORMAT_LETTER_FOUND_RIGHT_POSITION = 3;
   
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_GREEN = "\u001B[32m";
+  public static final String ANSI_YELLOW = "\u001B[33m";
+  public static final String ANSI_GREY = "\u001b[0m";
+
   final public static int GAME_WIN = 5;
   final public static int GAME_LOSE = 6;
   final public static int GAME_WAITING = 7;

823a63290cd4b580a7209279a9f234eb78fedef6
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2022-05-13 18:52:41 +0200

    Implementazione classe WordSmith
    Implementato il metodo setSecredWord e creata una nuova istanza di Game

--- src/main/java/it/uniba/app/control/WordSmith.java
+++ src/main/java/it/uniba/app/control/WordSmith.java
@@ -1,7 +1,26 @@
 package it.uniba.app.control;
 
+import it.uniba.app.models.Game;
 import it.uniba.app.utils.IWordSmith;
+import it.uniba.app.utils.WrongWordException;
 
+/**
+ * Classe che sarebbe il paroliere che serve per impostare e visualizzare la parola.
+ */
 public class WordSmith implements IWordSmith{
-    
+
+    private Game configuratedGame = new Game();
+
+    /** 
+     * Metodo che demanda al gameManager di impostare la parola.
+     * 
+     * @param word parola da impostare
+     * 
+    */
+    public void setSecretWord(String word) throws WrongWordException{
+		
+		  GameManager.setSecretWord(configuratedGame,word);
+		
+	  }
+
 }

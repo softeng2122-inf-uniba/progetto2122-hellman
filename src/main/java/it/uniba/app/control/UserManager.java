a5ddf6128e2a805701255fc3eb0066a43d1b63d0
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2022-05-13 18:51:19 +0200

    Implementazione classe UserManager

--- src/main/java/it/uniba/app/control/UserManager.java
+++ src/main/java/it/uniba/app/control/UserManager.java
@@ -1,8 +1,25 @@
 package it.uniba.app.control;
 
+import it.uniba.app.models.Game;
 import it.uniba.app.utils.IPlayer;
 import it.uniba.app.utils.IWordSmith;
+import it.uniba.app.utils.WrongWordException;
 
+/**
+ * Classe che serve per interfacciare l'utente con il game manager
+ */
 public class UserManager implements IPlayer, IWordSmith{
+    private Player player = new Player();
+    private WordSmith wordSmith = new WordSmith();
+    private Game currentGame = new Game();
 
+    /** 
+     * Metodo che demanda al paroliere l'impostazione della parola segreta.
+     * 
+     * @param word parola da impostare.
+    */
+    public void setSecretWord(String word) throws WrongWordException{
+        
+		wordSmith.setSecretWord(word);
+	}
 }

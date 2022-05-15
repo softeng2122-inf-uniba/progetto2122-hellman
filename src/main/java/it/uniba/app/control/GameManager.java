88113599c8c4ec25628f9c9108ed41df833efe18
Matteo Balice <matteobalice02@gmail.com>
2022-05-15 11:23:47 +0200

    Bug fix stampa parola segreta

--- src/main/java/it/uniba/app/control/GameManager.java
+++ src/main/java/it/uniba/app/control/GameManager.java
@@ -159,6 +159,7 @@ public class GameManager {
                 trys = currentGame.getTrys();
 
                 if(result == Helper.GAME_LOSE || result == Helper.GAME_WIN){
+                    trys.set(0, new Word(getSecretWord(currentGame), formats));
                     currentGame.resetGame();
                 }
                 

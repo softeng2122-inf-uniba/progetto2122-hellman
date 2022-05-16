9f7b53454b40ce982151b9d1db7d55a6851bb997
Matteo Balice <matteobalice02@gmail.com>
2022-05-16 19:05:41 +0200

    Commentata classe Viewer

--- src/main/java/it/uniba/app/view/Viewer.java
+++ src/main/java/it/uniba/app/view/Viewer.java
@@ -2,12 +2,17 @@ package it.uniba.app.view;
 import it.uniba.app.control.UserManager;
 
 /**
+ * <<Boundary>>
  * Classe astratta per gestire le varie interfacce di input
  */
 abstract class Viewer {
     
+    // Attributo control che gestisce l'utente(player e paroliere)
     UserManager usrManager;
 
+    /**
+     * Costruttore di viewer, inizializza usrManager
+     */
     public Viewer(){
         usrManager = new UserManager();
     }

30a0dcb487ad26816c773eb972e15c0d2718404b
Michele Spadaro <michele.141101@gmail.com>
2022-05-16 19:06:38 +0200

    Commenti Player

--- src/main/java/it/uniba/app/models/Player.java
+++ src/main/java/it/uniba/app/models/Player.java
@@ -5,13 +5,13 @@ package it.uniba.app.models;
  * Classe che identifica la figura del giocatore, contiene il game in stato di esecuzione.
  */
 public class Player{
-
+    /** Gioco che viene usato durante la partita corrente. */
     private Game currentGame = new Game();
 
     /**
      * Metodo che permette di far usare e modificare il Game attuale al UserManager
      * 
-     * @return ritorna il game configurabile
+     * @return ritorna il game corrente
      */
     public Game getCurrentGame() {
         return currentGame;

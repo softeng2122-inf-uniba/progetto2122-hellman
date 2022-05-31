e06c989a019a6739b6f0bc70dde435f3d4839859
Matteo Balice <matteobalice02@gmail.com>
2022-05-31 21:21:49 +0200

    Risolti checkstyle in Viewer.java

--- src/main/java/it/uniba/app/view/Viewer.java
+++ src/main/java/it/uniba/app/view/Viewer.java
@@ -1,22 +1,35 @@
 package it.uniba.app.view;
+
 import it.uniba.app.control.UserManager;
 
 /**
  * <<Boundary>>
- * Classe astratta per gestire le varie interfacce di input
+ * Classe astratta per gestire le varie interfacce di input.
  */
 abstract class Viewer {
-    
-    /** Attributo control che gestisce l'utente(player e paroliere) */
-    UserManager usrManager;
+
+    /** Attributo control che gestisce l'utente(player e paroliere). */
+    private UserManager usrManager;
 
     /**
-     * Costruttore di viewer, inizializza usrManager
+     * Costruttore di viewer, inizializza usrManager.
      */
-    public Viewer(){
+    Viewer() {
         usrManager = new UserManager();
     }
-    
-    /** Classe astratta da implementare in ogni classe derivata per avviare l'input */
-    abstract protected void readInput();
+
+    /**
+     * Classe astratta da implementare in ogni classe derivata
+     * per avviare l'input.
+     */
+    abstract void readInput();
+
+    /**
+     * Restituisce l'istanza di usrManager.
+     * 
+     * @return usrManager
+     */
+    UserManager getUserManager() {
+        return usrManager;
+    }
 }

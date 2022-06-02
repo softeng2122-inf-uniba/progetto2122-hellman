635222cb1aff11f6ab9df37326a261d0d1bf6d76
Matteo Balice <matteobalice02@gmail.com>
2022-06-02 23:28:22 +0200

    Risolti checkstyle WordSmith.java

--- src/main/java/it/uniba/app/models/WordSmith.java
+++ src/main/java/it/uniba/app/models/WordSmith.java
@@ -2,15 +2,17 @@ package it.uniba.app.models;
 
 /**
  * <<Entity>>
- * Classe che identifica la figura del paroliere, contiene il game in stato di configurazione.
+ * Classe che identifica la figura del paroliere, contiene il game in stato di
+ * configurazione.
  */
-public class WordSmith{
+public class WordSmith {
     /** Gioco che viene configurato prima dell'esecuzione. */
     private Game configuratedGame = new Game();
 
     /**
-     * Metodo che permette di far usare e modificare il Game configurabile al UserManager
-     * 
+     * Metodo che permette di far usare e modificare il Game configurabile al
+     * UserManager.
+     *
      * @return ritorna il game configurabile
      */
     public Game getConfiguratedGame() {

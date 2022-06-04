cf8d6a4b73457ad886b88bcfb4cf26b2ddaf7c8c
RR-UniBa <r.racamato@studenti.uniba.it>
2022-06-04 18:26:27 +0200

    Rimozione singoli metodi commands.add
    Rimozione dei singoli comandi di aggiunta nel costruttore del terminale per mancata aderenza al principio di Incapsulamento del software. Questo commit sarà completato con quello successivo di aggiunta metodo di incapsulamento.

--- src/main/java/it/uniba/app/view/Terminal.java
+++ src/main/java/it/uniba/app/view/Terminal.java
@@ -29,15 +29,6 @@ public class Terminal extends Viewer {
     public Terminal(final String[] flags) {
         super();
 
-        // Aggiungere i comandi accettati dal parser
-        commands.add(newCmd("/help", CommandType.HELP));
-        commands.add(newCmd("/esci", CommandType.EXIT_APP));
-        commands.add(newCmd("/gioca", CommandType.START_GAME));
-        commands.add(newCmd("si", CommandType.EXIT_YES));
-        commands.add(newCmd("no", CommandType.EXIT_NO));
-        commands.add(newCmd("/nuova", CommandType.NEW));
-        commands.add(newCmd("/mostra", CommandType.SHOW));
-        commands.add(newCmd("/abbandona", CommandType.EXIT_GAME));
 
         parser = new Parser(commands);
 

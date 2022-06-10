247e5889ac724d667d16f485c1fdd6d5582e73b5
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-10 21:27:11 +0200

    Aggiunta casi di test UserManager
    Aggiunta casi di test relativi ai metodi setSecretWord e getSecretWord di UserManager

--- src/test/java/it/uniba/app/control/UserManagerTest.java
+++ src/test/java/it/uniba/app/control/UserManagerTest.java
@@ -1,5 +1,72 @@
 package it.uniba.app.control;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import it.uniba.app.utils.GameException;
+
 public class UserManagerTest {
+    private UserManager userManager = new UserManager();
     
+    @Test
+    public void testSetSecretWord() {
+        String secretWord = "clock";
+
+        try {
+            userManager.setSecretWord(secretWord);
+        } catch (GameException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(secretWord, userManager.getSecretWord());
+    }
+
+    @Test
+    public void testSecretWordTooLong() {
+
+        Throwable exception = assertThrows(
+                GameException.class, () -> {
+                    userManager.setSecretWord("tooLong");
+                });
+
+        assertEquals("La parola inserita contiene un numero"
+                + " troppo elevato di caratteri.", exception.getMessage());
+    }
+
+    @Test
+    public void testSecretWordTooShort() {
+        Throwable exception = assertThrows(
+                GameException.class, () -> {
+                    userManager.setSecretWord("test");
+                });
+
+        assertEquals("La parola inserita contiene un numero insufficiente di "
+                + "caratteri.", exception.getMessage());
+    }
+
+    @Test
+    public void testSecretWordUnavailableCharacters() {
+        Throwable exception = assertThrows(
+                GameException.class, () -> {
+                    userManager.setSecretWord("te$st");
+                });
+
+        assertEquals("La parola contiene caratteri non ammessi.",
+                exception.getMessage());
+    }
+
+    @Test
+    public void testGetSecretWord() {
+        String secretWord = "hands";
+
+        try {
+            userManager.setSecretWord(secretWord);
+        } catch (GameException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(userManager.getSecretWord(), secretWord);
+    }
 }

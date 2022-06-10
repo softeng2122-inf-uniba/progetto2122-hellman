fcefa9e6f0f0bef87c8bfabbfe8411944fd2b1ed
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-10 20:06:36 +0200

    Creazione GameManagerTest

---
+++ src/test/java/it/uniba/app/control/GameManagerTest.java
@@ -0,0 +1,12 @@
+package it.uniba.app.control;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import it.uniba.app.utils.GameException;
+
+public class GameManagerTest {
+    
+}

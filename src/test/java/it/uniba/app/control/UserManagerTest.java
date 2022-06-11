74beed04b64a219b09046f5de717660a7ed89739
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-12 00:54:08 +0200

    Aggiunta casi di test startGame di UserManager

--- src/test/java/it/uniba/app/control/UserManagerTest.java
+++ src/test/java/it/uniba/app/control/UserManagerTest.java
@@ -1,15 +1,27 @@
 package it.uniba.app.control;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import it.uniba.app.models.Word;
 import it.uniba.app.utils.GameException;
+import it.uniba.app.utils.Helper;
+import it.uniba.app.utils.Pair;
+
 
 public class UserManagerTest {
+    
     private UserManager userManager = new UserManager();
-
+    
     @Test
     public void testSetSecretWord() {
         String secretWord = "clock";
@@ -69,4 +81,30 @@ public class UserManagerTest {
 
         assertEquals(userManager.getSecretWord(), secretWord);
     }
-}
+
+   
+    @Test
+    public void testStartGameConfiguratedGameNull() {
+        Throwable exception = assertThrows(
+                GameException.class, () -> {
+                    userManager.startGame();
+                });
+
+        assertEquals("Non è ancora stata inserita la parola segreta!",
+                exception.getMessage());
+    }
+
+    
+    @Test
+    public void testStartGameConfiguratedGameEmptySecretWord() {
+        Throwable exception = assertThrows(
+                GameException.class, () -> {
+                    userManager.startGame();
+                });
+
+        assertEquals("Non è ancora stata inserita la parola segreta!",
+                exception.getMessage());
+    }
+
+    
+    }

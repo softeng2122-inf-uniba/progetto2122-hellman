44ec65108d44371f30ef4acd69c9f560bf31eeb6
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-12 18:41:21 +0200

    Aggiunta casi di test relativi alla classe ParserOutput

--- src/test/java/it/uniba/app/view/ParserOutputTest.java
+++ src/test/java/it/uniba/app/view/ParserOutputTest.java
@@ -1,5 +1,48 @@
 package it.uniba.app.view;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniba.app.utils.CommandType;
+
 public class ParserOutputTest {
-    
+    private ParserOutput parserOutput;
+
+    @BeforeEach
+    public void testConstructor() {
+        String name = "/help";
+        CommandType type = CommandType.HELP;
+
+        Commands command = new Commands(type, name);
+
+        parserOutput = new ParserOutput(command);
+
+        assertEquals(command, parserOutput.getCommand());
+    }
+
+    @Test
+    public void testGetCommand() {
+        String name = "/gioca";
+        CommandType type = CommandType.START_GAME;
+
+        Commands command = new Commands(type, name);
+
+        parserOutput.setCommand(command);
+
+        assertEquals(command, parserOutput.getCommand());
+    }
+
+    @Test
+    public void testSetCommand() {
+        String name = "/nuova";
+        CommandType type = CommandType.NEW;
+
+        Commands command = new Commands(type, name);
+
+        parserOutput.setCommand(command);
+
+        assertEquals(parserOutput.getCommand(), command);
+    }
 }

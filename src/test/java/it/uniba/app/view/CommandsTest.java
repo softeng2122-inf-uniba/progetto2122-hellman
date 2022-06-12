5d86edc202d1fae246c502d09e1bfd3b82a0df78
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-12 18:40:54 +0200

    Aggiunta commenti Javadoc alla classe CommandsTest

--- src/test/java/it/uniba/app/view/CommandsTest.java
+++ src/test/java/it/uniba/app/view/CommandsTest.java
@@ -12,11 +12,27 @@ import org.junit.jupiter.api.Test;
 
 import it.uniba.app.utils.CommandType;
 
+/**
+ * Classe dedicata ai test dei metodi della classe Commands.
+ */
 public class CommandsTest {
+    /** Attributo da utilizzare per effettuare i casi di test. */
     private Commands commands;
+    /**
+     * Serve per impostare il nome del comando durante la creazione
+     *  dell'oggetto e durante il test del costruttore.
+     */
     private String name;
+    /**
+     * Serve per impostare il tipo del comando durante la creazione
+     *  dell'oggetto e durante il test del costruttore.
+     */
     private CommandType type;
 
+    /**
+     * Metodo per testare il comportamento del costruttore e per inizializzare
+     * i valori dell'oggetto commands da usare per i successivi casi di test.
+     */
     @BeforeEach
     public void testConstructor() {
         name = "/help";
@@ -28,6 +44,9 @@ public class CommandsTest {
             && commands.getName().equals(name));
     }
 
+    /**
+     * Metodo per il testing del metodo setAlias di Commands.
+     */
     @Test
     public void testSetAlias() {
         String[] alias = new String[] {"/h", "/aiuto"};
@@ -38,10 +57,13 @@ public class CommandsTest {
         assertEquals(aliasSet, commands.getAlias());
     }
 
+    /**
+     * Metodo per il testing del metodo getAlias di Commands.
+     */
     @Test
     public void testGetAlias() {
-        String[] alias = new String[] { "/h", "/aiuto" };
-        
+        String[] alias = new String[] {"/h", "/aiuto"};
+
         commands.setAlias(alias);
 
         Set<String> aliasSet = new HashSet<String>(Arrays.asList(alias));
@@ -49,11 +71,17 @@ public class CommandsTest {
         assertEquals(commands.getAlias(), aliasSet);
     }
 
+    /**
+     * Metodo per il testing del metodo getType di Commands.
+     */
     @Test
     public void testGetType() {
         assertEquals(commands.getType(), type);
     }
 
+    /**
+     * Metodo per il testing del metodo getName di Commands.
+     */
     @Test
     public void testGetName() {
         assertEquals(commands.getName(), name);

a0cfaff8defcc995e2ffbaa8320db4df73f49f1d
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-13 02:05:46 +0200

    rimozione import non utilizzati in AppTest

--- src/test/java/it/uniba/app/AppTest.java
+++ src/test/java/it/uniba/app/AppTest.java
@@ -1,14 +1,8 @@
 package it.uniba.app;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.jupiter.api.Test;
-
-import it.uniba.app.view.Terminal;
-
 /**
  * Main test class of the application.
  */
 public class AppTest {
-    
+
 }
